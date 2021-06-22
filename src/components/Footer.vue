f9a9ba2d0eb507dca61202340ef2a74974537276
Oscar <oscarrc.web@gmail.com>
2021-06-22 16:37:05 +0200

    [ADD] Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,12 +1,22 @@
 <template>
-    <v-container></v-container>
+    <v-bottom-navigation>
+      <v-btn value="check">
+        <span>Check</span>
+        <v-icon>mdi-check-all</v-icon>
+      </v-btn>
+      <v-btn value="solve">
+        <span>Solve</span>
+        <v-icon>mdi-flag-checkered</v-icon>
+      </v-btn>
+      <v-btn value="restart">
+        <span>Restart</span>
+        <v-icon>mdi-restart</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
 </template>
 
 <script>
   export default {
-    name: 'Footer',
-    data: () => ({
-      
-    }),
+    name: 'Footer'
   }
 </script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,10 +9,10 @@
       <v-chip outlined color="red">
         <span class="black--text">{{ timer }}</span>
       </v-chip>
-      <v-spacer></v-spacer>
+      <v-spacer class="d-none d-sm-flex"></v-spacer>
       <v-tooltip bottom>
         <template v-slot:activator="{ on: tooltip }">
-          <v-btn icon v-on="{ ...tooltip }">
+          <v-btn class="d-none d-sm-flex" icon v-on="{ ...tooltip }">
             <v-icon>mdi-check-all</v-icon>
           </v-btn>
         </template>
@@ -20,7 +20,7 @@
       </v-tooltip>
       <v-tooltip bottom>
         <template v-slot:activator="{ on: tooltip }">
-          <v-btn icon v-on="{ ...tooltip }" @click="showSolution">
+          <v-btn class="d-none d-sm-flex" icon v-on="{ ...tooltip }" @click="showSolution">
             <v-icon>mdi-flag-checkered</v-icon>
           </v-btn>
         </template>
@@ -28,7 +28,7 @@
       </v-tooltip>
       <v-tooltip bottom>
         <template v-slot:activator="{ on: tooltip }">
-          <v-btn icon v-on="{ ...tooltip }">
+          <v-btn class="d-none d-sm-flex" icon v-on="{ ...tooltip }">
             <v-icon>mdi-restart</v-icon>
           </v-btn>
         </template>
