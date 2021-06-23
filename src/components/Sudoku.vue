d693137e3cf88119ddef593f8327d08b40cd6549
Oscar <oscarrc.web@gmail.com>
2021-06-23 13:39:26 +0200

    [MOD] Set cell value

--- src/components/Sudoku.vue
+++ src/components/Sudoku.vue
@@ -3,7 +3,7 @@
     <v-row v-for="row, rindex in shown ? puzzle.solution : puzzle.grid" v-bind:key="rindex" class="justify-center">
       <v-col cols="1" v-for="col, cindex in row" v-bind:key="rindex + '-' + cindex" :class="'bordered d-flex justify-center ' + cellClasses(rindex, cindex)">
         <span v-if="col !== 0" class="cell text-center" >{{ col }}</span>
-        <input v-else type="number" class="cell text-center" @change="(e) => setCellValue(e) "/>
+        <input v-else type="number" class="cell text-center font-weight-bold" @keyup="(e) => setCellValue(rindex, cindex, e.target.value) "/>
       </v-col>
     </v-row>
   </v-container>
@@ -26,11 +26,12 @@
         let bottom = (r + 1) % 3 === 0 && r > 0 && r < (this.puzzle.grid.length - 1) ? 'border-b' : '';
         let right = (c  + 1) % 3 === 0 && c > 0 && c < (this.puzzle.grid[r].length - 1) ? 'border-r' : '';
         let text = this.shown && this.puzzle.grid[r][c] != this.puzzle.solution[r][c] ? 'red--text' : '';
-        let padding = this.puzzle.grid[r][c] != 0 ? 'py-1 py-sm-2 py-md-3 py-lg-5' : 'px-0 py-0'
+        let padding = this.puzzle.grid[r][c] != 0 || this.shown ? 'py-1 py-sm-2 py-md-3 py-lg-5' : 'px-0 py-0'
         return `${bottom} ${right} ${text} ${padding}`
       },
-      setCellValue(value){
-        console.log(value)
+      setCellValue(row, col, value){
+        value = value ? parseInt(value) : '';
+        if(value && !isNaN(value)) this.setCell({row, col, value});
       }
     }
   }
