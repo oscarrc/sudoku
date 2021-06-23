de575bb949aa2a6bd77238f7207bd3163fda5ce0
Oscar <oscarrc.web@gmail.com>
2021-06-23 12:49:47 +0200

    [TODO] Value selector

---
+++ src/components/Selector.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-dialog :value="active">
+    </v-dialog>
+</template>
+
+<script>
+  export default {
+    name: 'Selector',
+    data(){
+        return {
+            active : false, 
+            resolve: null
+        };
+    },
+    props:[values],
+    methods: {
+        
+    }
+  }
+</script>
