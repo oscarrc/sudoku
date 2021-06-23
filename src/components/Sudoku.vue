23611c2b497bd924d4fcb2710211a083df834d75
Oscar <oscarrc.web@gmail.com>
2021-06-23 17:06:55 +0200

    [MOD] remove error hint when cell value changes

--- src/components/Sudoku.vue
+++ src/components/Sudoku.vue
@@ -3,7 +3,7 @@
     <v-row v-for="row, rindex in shown ? sudoku.solution : sudoku.puzzle" v-bind:key="rindex" class="justify-center">
       <v-col cols="1" v-for="col, cindex in row" v-bind:key="rindex + '-' + cindex" :class="'bordered d-flex justify-center ' + cellClasses(rindex, cindex)">
         <span v-if="col !== 0" class="cell text-center">{{ col }}</span>
-        <input v-else type="number" :class="'cell text-center font-weight-bold '" @keyup="(e) => setCellValue(rindex, cindex, e.target.value) "/>
+        <input v-else type="number" :class="'cell text-center font-weight-bold ' + checkClasses(rindex,cindex)" @keyup="(e) => setCellValue(rindex, cindex, e) "/>
       </v-col>
     </v-row>
   </v-container>
@@ -27,15 +27,18 @@
         const right = (c  + 1) % 3 === 0 && c > 0 && c < (this.sudoku.grid[r].length - 1) ? 'border-r' : '';
         const padding = this.sudoku.puzzle[r][c] != 0 || this.shown ? 'py-1 py-sm-2 py-md-3 py-lg-5' : 'px-0 py-0';        
         const solve = this.shown ? ( this.sudoku.grid[r][c] != this.sudoku.solution[r][c] ? 'red--text font-weight-bold' : ( this.sudoku.puzzle[r][c] == 0 ? 'green--text font-weight-bold' : '' )) : '';
-        const check = this.checked && 
+
+        return `${bottom} ${right} ${solve} ${padding}`
+      },
+      checkClasses(r, c){
+        return this.checked && 
               this.sudoku.grid[r][c] != this.sudoku.solution[r][c] && 
               this.sudoku.puzzle[r][c] == 0 &&
               this.sudoku.grid[r][c] != 0 ? 'red lighten-4' : '';
-
-        return `${bottom} ${right} ${solve} ${padding} ${check}`
       },
-      setCellValue(row, col, value){
-        value = value ? parseInt(value) : '';
+      setCellValue(row, col, event){
+        let value = event.target.value ? parseInt(event.target.value) : '';
+        if(this.checked) event.target.classList.remove("red");
         if(value && !isNaN(value)) this.setCell({row, col, value});
       }
     }
