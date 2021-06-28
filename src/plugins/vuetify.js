6726203aae661578ec438e785f6329a29e059f37
Oscar <oscarrc.web@gmail.com>
2021-06-28 11:46:20 +0200

    [MOD] Vuetify theme colors

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,20 +4,18 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
-    
+    themes: {
+        light: {
+            primary: '#CF1920',
+            secondary: '#f78e1e',
+            accent: '#882528',
+            error: '#b71c1c',
+        },
+        dark: {
+            primary: '#3f51b5',
+            secondary: '#f78e1e',
+            accent: '#8c9eff',
+            error: '#b71c1c',
+        }
+    }
 });
-
-// themes: {
-//     light: {
-//         primary: '#3f51b5',
-//         secondary: '#b0bec5',
-//         accent: '#8c9eff',
-//         error: '#b71c1c',
-//     },
-//     dark: {
-//         primary: '#3f51b5',
-//         secondary: '#b0bec5',
-//         accent: '#8c9eff',
-//         error: '#b71c1c',
-//     }
-// }
