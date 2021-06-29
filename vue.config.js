d8332c19751fb99c0fe0de135b9f9f6a6e07217f
Oscar <oscarrc.web@gmail.com>
2021-06-29 10:49:48 +0200

    [MOD] PWA Vue config

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,25 @@ module.exports = {
   ],
   assetsDir: 'assets',
   pwa: {
-    name: '数独 Sudoku'
+    name: '数独 Sudoku',
+    short_name: '数独 Sudoku',
+    description: 'Procedurally generated and relaxing Sudoku game',
+    categories: ["Games", "Puzzle", "Sudoku"],
+    screenshots: [],
+    icons: [],
+    dir: "ltr",
+    lang: "en",
+    scope: "/",
+    start_url: ".",
+    display: "standalone",
+    theme_color: "#d11920",
+    background_color: "#ffffff",
+    orientation: "portrait",
+    related_applications: [
+      {
+        "platform": "web",
+        "url": "https://sudoku.oscarrc.me"
+      }
+    ]
   }
 }
