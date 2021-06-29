1a807c7b3092fb053ec985f5acf146e7f5578ad8
Oscar <oscarrc.web@gmail.com>
2021-06-29 10:32:02 +0200

    [MOD] Make sudoku grid square and responsive

--- src/components/Sudoku.vue
+++ src/components/Sudoku.vue
@@ -1,9 +1,11 @@
 <template>
   <v-container fluid>
     <v-row v-for="row, rindex in shown ? sudoku.solution : sudoku.puzzle" v-bind:key="rindex" class="justify-center">
-      <v-col cols="1" v-for="col, cindex in row" v-bind:key="rindex + '-' + cindex" :class="'elevation-4 frosted bordered d-flex justify-center ' + cellClasses(rindex, cindex)">
-        <span v-if="col !== 0" class="cell text-center">{{ col }}</span>
-        <input v-else type="number" :class="'cell text-center font-weight-bold ' + checkClasses(rindex,cindex)" @keyup="(e) => setCellValue(rindex, cindex, e) "/>
+      <v-col cols="1" v-for="col, cindex in row" v-bind:key="rindex + '-' + cindex" :class="'elevation-4 frosted bordered pa-0 ' + cellClasses(rindex, cindex)">
+        <v-responsive :aspect-ratio="1/1" class="text-center flex" content-class="d-flex justify-center align-self-stretch align-center" height="100%">
+          <span v-if="col !== 0">{{ col }}</span>
+          <input v-else type="number" :class="'cell text-center font-weight-bold ' + checkClasses(rindex,cindex)" @keyup="(e) => setCellValue(rindex, cindex, e) "/>
+        </v-responsive>
       </v-col>
     </v-row>
   </v-container>
@@ -24,11 +26,10 @@
       ...mapMutations(['setCell']),
       cellClasses(r, c){
         const bottom = (r + 1) % 3 === 0 && r > 0 && r < (this.sudoku.grid.length - 1) ? 'border-b' : '';
-        const right = (c  + 1) % 3 === 0 && c > 0 && c < (this.sudoku.grid[r].length - 1) ? 'border-r' : '';
-        const padding = this.sudoku.puzzle[r][c] != 0 || this.shown ? 'py-1 py-sm-2 py-md-3 py-lg-5' : 'px-0 py-0';        
+        const right = (c  + 1) % 3 === 0 && c > 0 && c < (this.sudoku.grid[r].length - 1) ? 'border-r' : '';      
         const solve = this.shown ? ( this.sudoku.grid[r][c] != this.sudoku.solution[r][c] ? 'red--text font-weight-bold' : ( this.sudoku.puzzle[r][c] == 0 ? 'green--text font-weight-bold' : '' )) : '';
-
-        return `${bottom} ${right} ${solve} ${padding}`
+        
+        return `${bottom} ${right} ${solve}`
       },
       checkClasses(r, c){
         return this.checked && 
@@ -61,10 +62,12 @@
     border-right-width: 2px !important;
     border-right-color: black;
   }
+  .cell-wrapper{
+    min-height: 100% !important
+  }
   .cell {
-    width: inherit;
-    height: inherit;
-    min-height: 1.5rem;
+    width: 100%;
+    height: 100%;
   }
   input::-webkit-outer-spin-button,
   input::-webkit-inner-spin-button {
