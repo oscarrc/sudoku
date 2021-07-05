f69157dec180c2dee73260a763d1eaf73f9063cf
Oscar <oscarrc.web@gmail.com>
2021-07-05 17:21:23 +0200

    [FIX] Cannot read property of undefined values

--- src/components/Selector.vue
+++ src/components/Selector.vue
@@ -4,7 +4,7 @@
           <v-row>
             <v-col v-for="i in 9" cols="4" v-bind:key="i" class="pa-0">
                 <v-responsive :aspect-ratio="1/1" class="text-center">
-                    <v-btn :class="values[i] === true ? 'text--secondary' : ''" block tile max-width="100%" min-height="100%" @click="() => setValue(i)">
+                    <v-btn :class="values[i] && values[i] === true ? 'text--secondary' : ''" block tile max-width="100%" min-height="100%" @click="() => setValue(i)">
                         {{ i }}
                     </v-btn>
                 </v-responsive>
