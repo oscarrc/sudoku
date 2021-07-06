44007eeb2d4e523e37db62352d2a8796d82a5c6d
Oscar <oscarrc.web@gmail.com>
2021-07-06 08:59:17 +0200

    [MOD] Remove old code

--- src/components/Sudoku.vue
+++ src/components/Sudoku.vue
@@ -5,10 +5,6 @@
         <v-responsive :aspect-ratio="1/1" class="text-center flex" content-class="d-flex justify-center align-self-stretch align-center" height="100%">
           <span v-if="col !== 0">{{ col }}</span>
           <Cell v-else :values="sudoku.grid[rindex][cindex]" @click="(e) => showSelector(e, rindex, cindex)" :class="checkClasses(rindex, cindex)" />
-          <!-- <span v-else @click="(e) => showSelector(e, rindex, cindex)" :class="'d-flex justify-center align-center cell font-weight-bold ' + checkClasses(rindex,cindex)">
-            {{ sudoku.grid[rindex][cindex] ? sudoku.grid[rindex][cindex] : '' }}
-          </span> -->
-          <!-- <input v-else type="number" :class="'cell text-center font-weight-bold ' + checkClasses(rindex,cindex)" @keyup="(e) => setCellValue(rindex, cindex, e) "/> -->
         </v-responsive>
       </v-col>
     </v-row>
@@ -54,11 +50,6 @@
               value[0] != this.sudoku.solution[r][c] && 
               this.sudoku.puzzle[r][c] == 0 ? 'error lighten-4' : '';
       },
-      // setCellValue(row, col, event){
-      //   let value = event.target.value ? parseInt(event.target.value) : 0;
-      //   if(this.checked) event.target.classList.remove("error");
-      //   if(value && !isNaN(value)) this.setCell({row, col, value});
-      // },
       setCellValue(value){
         this.setCell({row: this.cell[0], col: this.cell[1], value});
       },
@@ -92,12 +83,4 @@
     border-right-width: 2px !important;
     border-right-color: black;
   }
-  input::-webkit-outer-spin-button,
-  input::-webkit-inner-spin-button {
-    -webkit-appearance: none;
-    margin: 0;
-  }
-  /* input[type=number] {
-    -moz-appearance:textfield;
-  } */
 </style>
