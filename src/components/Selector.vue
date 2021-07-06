772a8ff22b527f0bf5f55b5ced52ca42e6b20c49
Oscar <oscarrc.web@gmail.com>
2021-07-06 08:49:34 +0200

    [FIX] Values are undefined

--- src/components/Selector.vue
+++ src/components/Selector.vue
@@ -1,10 +1,10 @@
 <template>
-    <v-menu min-width="120px" max-width="120px" :close-on-content-click="true" :value="open" :position-x="position[0]" :position-y="position[1]">
+    <v-menu min-width="120px" max-width="120px" :close-on-content-click="false" :value="open" :position-x="position[0]" :position-y="position[1]">
       <v-container fluid class="white">
-          <v-row>
+          <v-row v-if="values">
             <v-col v-for="i in 9" cols="4" v-bind:key="i" class="pa-0">
                 <v-responsive :aspect-ratio="1/1" class="text-center">
-                    <v-btn :class="values[i] && values[i] === true ? 'text--secondary' : ''" block tile max-width="100%" min-height="100%" @click="() => setValue(i)">
+                    <v-btn :class="values[i] == true ? 'v-item--active v-btn--active text--secondary' : ''" block tile max-width="100%" min-height="100%" @click="() => setValue(i)">
                         {{ i }}
                     </v-btn>
                 </v-responsive>
